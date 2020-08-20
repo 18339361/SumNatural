04d1b6144a07a98af421276e09be0fe63ebe5496
18339361 <pranchal2001@gmail.com>
2020-08-20 18:07:17 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int sum=0;
+        for(int i=1; i<=100; i++){
+            sum+=i;
+        }
+        System.out.println("The sum of all the natural numbers between 1-100 is: " + sum);
+    }
+}
